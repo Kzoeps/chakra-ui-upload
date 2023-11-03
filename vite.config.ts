8e744db2d6813e3f0cfb7777e737b78da0b13fe3
kzoeps <kzoepa@gmail.com>
2023-11-03 16:45:07 -0400

    move to lib

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: './src/lib/test/setup.ts',
     css: false
   }
 })
